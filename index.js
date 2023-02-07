98d3d47a643529bf2002d8d2c0df7b9b56347dcd
LokosFero <bilardoraka64@gmail.com>
2023-02-07 20:35:24 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+
+const app = express();
+let cors = require("cors");
+app.use(cors());
+
+async function getKurs() {
+  try {
+    const siteUrl =
+      "https://kurs.dollar.web.id/kurs-transaksi-bank-indonesia.php";
+    const { data } = await axios({
+      method: "GET",
+      url: siteUrl,
+    });
+
+    const $ = cheerio.load(data);
+    const elemSelector = "#main > div > div:nth-child(7) > table > tbody > tr";
+    const keys = ["Valas", "HargaBeli", "HargaJual"];
+    const kursArr = [];
+
+    $(elemSelector).each((parentIdx, parentElem) => {
+      let keyIdx = 0;
+      const kursObj = {};
+
+      $(parentElem)
+        .children()
+        .each((childIdx, childElem) => {
+          const tdValue = $(childElem).text();
+          if (tdValue) {
+            kursObj[keys[keyIdx]] = tdValue;
+            keyIdx++;
+          }
+        });
+      kursArr.push(kursObj);
+    });
+    return kursArr;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+app.get("/", async (req, res) => {
+  try {
+    const kursFeed = await getKurs();
+    return res.status(200).json({
+      kursFeed,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      err: err.toString(),
+    });
+  }
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("running on port", port);
+});
