d9e587774f24762263e8aebd73367851d8368ab1
LokosFero <bilardoraka64@gmail.com>
2023-02-07 20:46:02 +0700

    PORT

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ app.get("/", async (req, res) => {
 });
 
 // PORT
-app.listen(process.env.PORT || 3000, () => {
-  console.log("running on port", port);
+const server = app.listen(process.env.PORT || 5000, () => {
+  const port = server.address().port;
+  console.log(`Express is working on port ${port}`);
 });
